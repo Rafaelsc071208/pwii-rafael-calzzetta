dcfdf000010050682000512d17391f2a402cc4c8
Rafael Serafim <rafaelserafimcalzzetta@gmail.com>
2025-02-19 10:34:00 -0300

    Aula de introdução ao PHP

---
+++ Aula01/teste.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        .f1{color: red;
+        font-size: 4cm;
+    background-color: blue;}
+
+    
+    
+    
+    
+    </style>
+ <p class="f1">
+<?php 
+echo  "My first PHP script!";
+?>
+</p>
+</body>
+</html>
