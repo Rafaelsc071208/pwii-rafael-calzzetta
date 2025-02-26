fa6a75fb58b8597d80c8c8246d384629e8002a44
Rafael Serafim <rafaelserafimcalzzetta@gmail.com>
2025-02-26 10:28:35 -0300

    Aula sobre variaveis

---
+++ Aula02/concatenar.php
@@ -0,0 +1,32 @@
+<?php
+echo "Hello World<br>";
+
+$nome = "Rafael";
+$sobrenome = "Serafim";
+$numero = "4";
+$numero2 = "5";
+
+echo "<p>${nome} <h1>${sobrenome}</h1></p>";
+
+echo "${numero}"+"${numero2}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
