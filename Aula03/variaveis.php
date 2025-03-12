4fc62215de9d5f0bbd3e3161bc32072099c7a6be
Rafael Serafim <rafaelserafimcalzzetta@gmail.com>
2025-03-12 10:15:41 -0300

    Aula com variaveis

---
+++ Aula03/variaveis.php
@@ -0,0 +1,17 @@
+<?php
+$texto = "Texto genérioco";
+$n1 = 2;
+$n2 = 4;
+
+
+echo $texto;
+echo "<br>";
+echo gettype($texto), "<br>";
+echo ($n1+$n2),"<br>";
+echo gettype($n1), "<br>";
+
+
+
+
+
+?>
