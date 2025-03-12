ee7e3c56a810153bfb13bde276512711e3cfad63
Rafael Serafim <rafaelserafimcalzzetta@gmail.com>
2025-03-12 10:22:28 -0300

    aula de decisao

---
+++ Aula03/decisao.php
@@ -0,0 +1,23 @@
+<?php
+if ($n >= 4) {
+    echo $n, "é maior ou igual a 4 + uma alteração quase plausivel";
+}
+$n1 = 3;
+$n2 = 4;
+$result = ($n1%$n2)%2;
+
+if ($result ==1) {
+    echo "<br>",$result,"quociente impar de novo";
+} else{
+    echo"<br>",$result,"quociente de par";
+}
+
+
+
+
+
+
+
+
+
+?>
