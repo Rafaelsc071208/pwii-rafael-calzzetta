e78119cde58af1e3b56069939eec990daaf3f3b2
Rafael Serafim <rafaelserafimcalzzetta@gmail.com>
2025-03-19 10:28:26 -0300

    usando for, do e while para repetição php

---
+++ Aula04/repetcao.php
@@ -0,0 +1,37 @@
+<?php
+$i = 1;
+echo("Contagem progressiva enquanto o número for menor que 6: ");
+echo("<br>");
+while ($i <6){
+    echo $i;
+    echo "<br>";
+    $i++;
+}
+$b = 1;
+echo("<br>");
+echo ("____________________________________");
+echo("<br>");
+do {
+    echo$b;
+    $b++;
+}while ($b < 6);
+echo("<br>____________________________________<br>");
+for ($x = 0; $x <= 10; $x++){
+    echo "O número é: $x <br>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
