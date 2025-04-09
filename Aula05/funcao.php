306a134c79137baf268a2e6941afa3797abb227a
Rafael Serafim <rafaelserafimcalzzetta@gmail.com>
2025-04-09 10:32:11 -0300

    Usando funções em php

---
+++ Aula05/funcao.php
@@ -0,0 +1,18 @@
+<?php
+
+
+function myMessage(){
+    echo  "Hello world <br>";
+}
+
+myMessage();
+myMessage();
+
+function Soma($n1, $n2){
+    echo $n1+$n2, "<br>";
+}
+
+Soma(3, 4);
+Soma(5, 5);
+
+?>
